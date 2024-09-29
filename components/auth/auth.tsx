a8ee68bb141ae72e21a62ddf0578c584fd725e02
AvichalDwivedi2205 <150160561+AvichalDwivedi2205@users.noreply.github.com>
2024-09-30 03:10:37 +0530

    Setting up authentication frontend and backend

--- components/auth/auth.tsx
+++ components/auth/auth.tsx
@@ -1,99 +1,116 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
-import {auth, googleProvider} from "@/config/firebase"
+import { auth, googleProvider } from "@/config/firebase";
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
-import { div, form } from "framer-motion/client";
 import { IconBrandGoogle } from "@tabler/icons-react";
 
 const AuthForm = ({ isSignUp }: { isSignUp: boolean }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassowrd] = useState("");
-    const [error, setError] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const handleSubmit = async(e:React.FormEvent) => {
-        try{
-             if(isSignUp){
-                await createUserWithEmailAndPassword(auth, email, password);
-             }else{
-                signInWithEmailAndPassword(auth, email, password);
-             }
-        }catch(err: any){
-            setError(err.message);
-        }
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); // Prevent the form from reloading the page
+    try {
+      if (isSignUp) {
+        await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        await signInWithEmailAndPassword(auth, email, password); // Add await here
+      }
+    } catch (err: any) {
+      setError(err.message);
     }
+  };
 
-    const handleGoogleSignIn = async () => {
-        try{
-            await signInWithPopup(auth, googleProvider);
-        }catch (err: any){
-            setError(err.message);
-        }
+  const handleGoogleProvider = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err: any) {
+      setError(err.message);
     }
+  };
 
-    return (
-        <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-            <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-                Welcome to SneakBid
-            </h2>
-            <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-                Step Into The Bidding Game
-            </p>
-            <form className="my-8" onSubmit={handleSubmit}>
-            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                <LabelInputContainer>
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    />
-                </LabelInputContainer>
-                <LabelInputContainer>
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    />
-                </LabelInputContainer>
-            </div>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+      <h2 className="text-center font-bold text-xl text-neutral-800 dark:text-neutral-200">
+        Welcome to SneakBid
+      </h2>
+      <p className="text-center text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        Step Into The Bidding Game
+      </p>
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      <form className="my-8" onSubmit={handleSubmit}>
+        <LabelInputContainer className="py-8">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="avichaldwivedi@gmail.com"
+            required
+          />
+        </LabelInputContainer>
+        <LabelInputContainer>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="••••••••"
+            required
+          />
+        </LabelInputContainer>
+        <button
+          className="mt-8 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          {isSignUp ? "Sign Up" : "Sign In"}
+          <BottomGradient />
+        </button>
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+        <button
+          className="mt-8 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="button" // Type should be button for Google Sign-in
+          onClick={handleGoogleProvider}
+        >
+          Sign In with Google
+          <BottomGradient />
+        </button>
+      </form>
+    </div>
+  );
+};
 
 const BottomGradient = () => {
-    return (
-      <>
-        <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-        <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-      </>
-    );
-  };
-   
-  const LabelInputContainer = ({
-    children,
-    className,
-  }: {
-    children: React.ReactNode;
-    className?: string;
-  }) => {
-    return (
-      <div className={cn("flex flex-col space-y-2 w-full", className)}>
-        {children}
-      </div>
-    );
-  };
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
 
-export default AuthForm
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default AuthForm;
